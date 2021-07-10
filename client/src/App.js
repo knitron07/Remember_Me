e66bdae09f66554288e155f45f74b2fa7ed17686
knitron07 <samarmahur1992@gmail.com>
2021-07-10 23:02:00 +0530

    add readme.md file

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Home from "./pages/home/Home";
-
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Profile from "./pages/Profile/profile";
 
 function App() {
   return (
-    <Home />
+    <Profile />
   );
 }
 
